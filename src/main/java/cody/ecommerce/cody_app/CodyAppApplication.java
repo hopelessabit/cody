b58fc510658470fc340fd1b3426da3d0036a8d26
hopelessabit <micalminh1@gmail.com>
2025-08-08 16:49:04 +0700

    init back end

---
+++ src/main/java/cody/ecommerce/cody_app/CodyAppApplication.java
@@ -0,0 +1,13 @@
+package cody.ecommerce.cody_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodyAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodyAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cody/ecommerce/cody_app/CodyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package cody.ecommerce.cody_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodyAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
