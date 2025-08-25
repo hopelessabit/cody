63c72dfe8213ec985f3481b4d9f000ac58dad4bb
hopelessabit <micalminh1@gmail.com>
2025-08-25 15:28:19 +0700

    Fix

--- src/main/java/cody/ecommerce/cody_app/config/SecurityConfig.java
+++ src/main/java/cody/ecommerce/cody_app/config/SecurityConfig.java
@@ -119,8 +119,7 @@ public class SecurityConfig {
                 "http://localhost:5174",
                 "http://localhost:5173",
                 "https://localhost:5174",
-                "https://localhost:5173",
-                "https://localhost:8080"));
+                "https://localhost:5173"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
         configuration.setAllowCredentials(true); // Allow credentials
